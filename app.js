9b8c475da6a00d23e75b7d759bbccd99cbbe48e7
DarioMeliaBasus <dario@meliabasus.com>
2021-08-10 19:18:15 +0200

    add background, rename adminScript.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
-const ejs = require("ejs");
+
 
 const app = express();
 
