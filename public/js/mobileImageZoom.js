8a991ecfa8a736658c418334d053a82d334cc6b9
DarioMeliaBasus <dario@meliabasus.com>
2021-11-09 15:40:01 +0100

    Implement zoom on tap and movement on drag

--- public/js/mobileImageZoom.js
+++ public/js/mobileImageZoom.js
@@ -1,85 +1,82 @@
-const images = document.querySelectorAll(".obra-info__img > img");
+var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    vx = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    vy = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
 
 
-images.forEach(image => {
-const minScale = 1;
-const maxScale = 12;
-let imageScale = 1;
-  
-let rangeMaxX = 0;
-let rangeMinX = 0;
-
-let rangeMaxY = 0;
-let rangeMinY = 0;
-const hammertime = new Hammer.Manager(image, {
-    touchAction: "auto",
-    recognizers: [
-      [Hammer.Pinch, { enable: true }],
-      [Hammer.Tap]
-    ]
-  });
-
-  
-  hammertime.on('tap', event => {
-      console.log('tap', event);
-    });
-  hammertime.on('pinchstart', event => {
-    console.log('pinchstart', event);
-  });
-
-  hammertime.on('pinchmove', event => {
-    // console.log('pinchmove', event);
-    imageScale = Math.min(Math.max(minScale, event.scale), maxScale);
-    updateRange();
-    updateImage(event.deltaX, event.deltaY);
-  });
-
-  hammertime.on('pinchend', event => {
-    console.log('pinchend', event);
-    resetImage();
-  });
+    if(vx <= 450){
+        makeImageZoom();
+    }
 
 
 
-    const imageWidth = image.offsetWidth;
-    const imageHeight = image.offsetHeight;
-  
 
 
-  image.parentNode.style.overflow = 'visible';
-  image.classList.add('pz-Image');
+    function makeImageZoom() {
+        const images = document.querySelectorAll(".obra-info__img > img");
 
- 
 
-  const updateRange = () => {
-    const rangeX = Math.max(0, Math.round(imageWidth * imageScale) - imageWidth);
-    const rangeY = Math.max(0, Math.round(imageHeight * imageScale) - imageHeight);
-    rangeMaxX = Math.round(rangeX / 2);
-    rangeMinX = Math.round(0 - rangeMaxX);
-
-    rangeMaxY = Math.round(rangeY / 2);
-    rangeMinY = Math.round(0 - rangeMaxY);
-  };
-
-  const updateImage = (deltaX, deltaY) => {
-    const imageCurrentX = Math.min(Math.max(rangeMinX, deltaX), rangeMaxX) * 2;
-    const imageCurrentY = Math.min(Math.max(rangeMinY, deltaY), rangeMaxY) * 2;
-
-    const transform = `translate3d(${imageCurrentX}px, ${imageCurrentY}px, 0) scale(${imageScale})`;
-    image.style.transform = transform;
-    image.style.WebkitTransform = transform;
-    image.style.zIndex = "9999";
-  };
-
-  const resetImage = () => {
-    image.style.transform = "";
-    image.style.WebkitTransform = "";
-    image.style.zIndex = "";
-  };
-
-  
-
-})
+    images.forEach(image => {
+        image.parentNode.style.overflow = 'visible';
+        image.classList.add('pz-Image');
+            
+        const hammertime = new Hammer(image);
+        hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+        
+        let isBig = false;
+        
+        hammertime.on('tap', event => {
+            console.log('tap', event);
+            if(isBig){
+              image.style.transform = "scale(1) translate(0 ,0)";
+              isBig = false;
+            }else{
+              image.style.transform = "scale(2.5)";
+              isBig = true;
+            }
+          });
+        
+        
+          hammertime.on("panmove", handleDrag);
+          hammertime.on("panend", ev => {
+              console.log("panENd");
+            //   ev.target.style.transform = "translate(0 ,0) scale(1)";
+            setTimeout(() => {
+                ev.target.style.transform = "translate(0 ,0) scale(1)";
+                isBig = false}, 2000)
+          });
+        
+        
+        
+        var lastPosX = 0;
+        var lastPosY = 0;
+        
+        function handleDrag(ev) {
+        
+            console.log('drag', ev.deltaX, ev.deltaY)
+           
+          // for convience, let's get a reference to our object
+          var elem = ev.target;
+          
+            lastPosX = elem.offsetLeft;
+            lastPosY = elem.offsetTop;
+        
+          var posX = ev.deltaX + lastPosX;
+          var posY = ev.deltaY + lastPosY;
+          
+          // move our element to that position
+          isBig = true;
+          elem.style.transform = `translateX(${posX}px) translateY(${posY}px) scale(2.5) `;
+        
+        
+        }
+          
+        
+        })
+        
+    }
 
 
 
