29d1a8a5dbd5971647b7126c18f7d95bc8a31a53
DarioMeliaBasus <dario@meliabasus.com>
2021-11-09 20:41:01 +0100

    Finish implementing zoom, on drag and on pinch, also tap for images in obra-info. Hammer.js

--- public/js/mobileImageZoom.js
+++ public/js/mobileImageZoom.js
@@ -1,83 +1,95 @@
 var win = window,
-    doc = document,
-    docElem = doc.documentElement,
-    body = doc.getElementsByTagName('body')[0],
-    vx = win.innerWidth || docElem.clientWidth || body.clientWidth,
-    vy = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
+  doc = document,
+  docElem = doc.documentElement,
+  body = doc.getElementsByTagName("body")[0],
+  vx = docElem.clientWidth || body.clientWidth,
+  vy = docElem.clientHeight || body.clientHeight;
 
+if (vx <= 450) {
+  makeImageZoom();
+}
 
-    if(vx <= 450){
-        makeImageZoom();
+function debounce(func) {
+  var timer;
+  return function (event) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, 100, event);
+  };
+}
+window.addEventListener(
+  "resize",
+  debounce(function (e) {
+    console.log("end of resizing");
+    (vx = win.innerWidth || docElem.clientWidth || body.clientWidth),
+      (vy = win.innerHeight || docElem.clientHeight || body.clientHeight);
+  })
+);
+
+function makeImageZoom() {
+  const images = document.querySelectorAll(".obra-info__img > img");
+
+  images.forEach((image) => {
+    image.parentNode.style.overflow = "visible";
+
+    const hammertime = new Hammer(image);
+    hammertime.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+    hammertime.get("pinch").set({ enable: true });
+
+    let isBig = false;
+
+    hammertime.on("tap", (event) => {
+      if (isBig) {
+        image.style.transform = "translate(0 ,0)";
+        image.parentElement.classList.remove("expanded");
+        isBig = false;
+      } else {
+        image.style.transform = "translate(0, 40px) ";
+        image.parentElement.classList.add("expanded");
+        isBig = true;
+      }
+    });
+
+    hammertime.on("panmove", handleDrag);
+    hammertime.on("panend", (ev) => {
+      console.log("panENd");
+      setTimeout(() => {
+        ev.target.style.transform = "translate(0 ,0)";
+        ev.target.parentElement.classList.remove("expanded");
+        isBig = false;
+      }, 100);
+    });
+
+    hammertime.on("pinchmove", handleDrag);
+    hammertime.on("pinchend", (ev) => {
+      setTimeout(() => {
+        ev.target.style.transform = "translate(0 ,0)";
+        ev.target.parentElement.classList.remove("expanded");
+        isBig = false;
+      }, 100);
+    });
+
+    function handleDrag(ev) {
+      
+      var elem = ev.target;
+      var posX, posY;
+      posX = ev.deltaX;
+      posY = ev.deltaY;
+
+      let maxX = (135 * vx) / 450; //Regla de 3 para sacar maximos y minimos teniendo en cuenta tamaño de 450/750
+      let minX = (-135 * vx) / 450;
+      let maxY = (89 * vy) / 750;
+      let minY = (36 * vy) / 750;
+      //Si se sale de los limites se queda en el limite
+      if (posX > maxX) posX = maxX;
+      if (posX < minX) posX = minX;
+      if (posY > maxY) posY = maxY;
+      if (posY < minY) posY = minY;
+
+      // move our element to that position
+      isBig = true;
+      firstPanMove = false;
+      elem.style.transform = `translateX(${posX}px) translateY(${posY}px)`;
+      elem.parentElement.classList.add("expanded");
     }
-
-
-
-
-
-    function makeImageZoom() {
-        const images = document.querySelectorAll(".obra-info__img > img");
-
-
-    images.forEach(image => {
-        image.parentNode.style.overflow = 'visible';
-        image.classList.add('pz-Image');
-            
-        const hammertime = new Hammer(image);
-        hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
-        
-        let isBig = false;
-        
-        hammertime.on('tap', event => {
-            console.log('tap', event);
-            if(isBig){
-              image.style.transform = "scale(1) translate(0 ,0)";
-              isBig = false;
-            }else{
-              image.style.transform = "scale(2.5)";
-              isBig = true;
-            }
-          });
-        
-        
-          hammertime.on("panmove", handleDrag);
-          hammertime.on("panend", ev => {
-              console.log("panENd");
-            //   ev.target.style.transform = "translate(0 ,0) scale(1)";
-            setTimeout(() => {
-                ev.target.style.transform = "translate(0 ,0) scale(1)";
-                isBig = false}, 2000)
-          });
-        
-        
-        
-        var lastPosX = 0;
-        var lastPosY = 0;
-        
-        function handleDrag(ev) {
-        
-            console.log('drag', ev.deltaX, ev.deltaY)
-           
-          // for convience, let's get a reference to our object
-          var elem = ev.target;
-          
-            lastPosX = elem.offsetLeft;
-            lastPosY = elem.offsetTop;
-        
-          var posX = ev.deltaX + lastPosX;
-          var posY = ev.deltaY + lastPosY;
-          
-          // move our element to that position
-          isBig = true;
-          elem.style.transform = `translateX(${posX}px) translateY(${posY}px) scale(2.5) `;
-        
-        
-        }
-          
-        
-        })
-        
-    }
-
-
-
-
+  });
+}
