6b0af2c4bb23209a9511f96bb832ec1ba45ab506
DarioMeliaBasus <dario@meliabasus.com>
2021-11-10 12:43:35 +0100

    Initial commit

--- route-controllers/home.js
+++ route-controllers/home.js
@@ -1,24 +1,21 @@
 const Obra = require("../models/obra.js");
 
-exports.homeGet = async (req,res) => {
-    try{
-        const lastObras = await Obra.find().sort({ createdAt: -1 }).limit(4);
-        
-        res.render("home", {obras:lastObras});
-    }catch(err){
-        res.send(err.message);
-    }
- 
-}
+exports.homeGet = async (req, res) => {
+  try {
+    const lastObras = await Obra.find().sort({ createdAt: -1 }).limit(4);
 
+    res.render("home", { obras: lastObras });
+  } catch (err) {
+    res.send(err.message);
+  }
+};
 
-exports.obrasGet = async (req,res) => {
-    try{
-        const obras = await Obra.find();
+exports.obrasGet = async (req, res) => {
+  try {
+    const obras = await Obra.find();
 
-        res.render("obras",{obras:obras});
-    }catch(err){
-        res.send(err.message);
-    }
-
-}
+    res.render("obras", { obras: obras });
+  } catch (err) {
+    res.send(err.message);
+  }
+};
