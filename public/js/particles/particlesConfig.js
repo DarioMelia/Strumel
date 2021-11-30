89bcb25257066c76532c3455acb6a09d25d7239d
DarioMeliaBasus <dario@meliabasus.com>
2021-11-30 17:45:28 +0100

    Improve accesibility and SEO for obras page

--- public/js/particles/particlesConfig.js
+++ public/js/particles/particlesConfig.js
@@ -55,7 +55,7 @@ particlesJS('particles-js',
     },
     "move": {
       "enable": true,
-      "speed": 3,
+      "speed": 1.6,
       "direction": "none",
       "random": true,
       "straight": false,
