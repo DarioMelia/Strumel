b3ea4b63b021733a9550b119d766d6d6516d568d
DarioMeliaBasus <dario@meliabasus.com>
2021-12-18 23:51:13 +0100

    Detect browser with bowser api, implement alerts for version not supporting newest css

--- public/js/browserDetection.js
+++ public/js/browserDetection.js
@@ -1,71 +1,40 @@
-var nVer = navigator.appVersion;
-var nAgt = navigator.userAgent;
-var browserName  = navigator.appName;
-var fullVersion  = ''+parseFloat(navigator.appVersion); 
-var majorVersion = parseInt(navigator.appVersion,10);
-var nameOffset,verOffset,ix; 
+var result = bowser.getParser(window.navigator.userAgent);
 
 
-// In Opera 15+, the true version is after "OPR/" 
-if ((verOffset=nAgt.indexOf("OPR/"))!=-1) {
- browserName = "Opera";
- fullVersion = nAgt.substring(verOffset+4);
-}
-// In older Opera, the true version is after "Opera" or after "Version"
-else if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
- browserName = "Opera";
- fullVersion = nAgt.substring(verOffset+6);
- if ((verOffset=nAgt.indexOf("Version"))!=-1) 
-   fullVersion = nAgt.substring(verOffset+8);
-}
-// In MSIE, the true version is after "MSIE" in userAgent
-else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
- browserName = "Microsoft Internet Explorer";
- fullVersion = nAgt.substring(verOffset+5);
-}
-// In Chrome, the true version is after "Chrome" 
-else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
- browserName = "Chrome";
- fullVersion = nAgt.substring(verOffset+7);
-}
-// In Safari, the true version is after "Safari" or after "Version" 
-else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
- browserName = "Safari";
- fullVersion = nAgt.substring(verOffset+7);
- if ((verOffset=nAgt.indexOf("Version"))!=-1) 
-   fullVersion = nAgt.substring(verOffset+8);
-}
-// In Firefox, the true version is after "Firefox" 
-else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
- browserName = "Firefox";
- fullVersion = nAgt.substring(verOffset+8);
-}
-// In most other browsers, "name/version" is at the end of userAgent 
-else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
-          (verOffset=nAgt.lastIndexOf('/')) ) 
-{
- browserName = nAgt.substring(nameOffset,verOffset);
- fullVersion = nAgt.substring(verOffset+1);
- if (browserName.toLowerCase()==browserName.toUpperCase()) {
-  browserName = navigator.appName;
- }
-}
-// trim the fullVersion string at semicolon/space if present
-if ((ix=fullVersion.indexOf(";"))!=-1)
-   fullVersion=fullVersion.substring(0,ix);
-if ((ix=fullVersion.indexOf(" "))!=-1)
-   fullVersion=fullVersion.substring(0,ix);
+var os = result.parsedResult.os.name;
+var browser = result.parsedResult.browser.name;
+var version = parseInt(result.parsedResult.browser.version.slice(0,2));
+var platform = result.parsedResult.platform.type
+console.log(os,browser,version, platform)
+if(platform === "desktop"){
+  if(browser === "Chrome" && version <= 83){
+    alert("Version antigua de Chrome en ordenador")
+  }
+  if(browser === "Microsoft Edge" && version <= 83){
+    alert("Version antigua de Edge en ordenador")
+  }
+  if(browser === "Firefox" && version <= 74){
+    alert("Version antigua de Firefox en ordenador")
+  }
+  if(browser === "Safari" && version <= 14){
+    alert("Version antigua de Safari en ordenador")
+  }
+  if(browser === "Opera" && version <= 72){
+    alert("Version antigua de Opera en ordenador")
+  }
 
-majorVersion = parseInt(''+fullVersion,10);
-if (isNaN(majorVersion)) {
- fullVersion  = ''+parseFloat(navigator.appVersion); 
- majorVersion = parseInt(navigator.appVersion,10);
+}else if(platform === "mobile"){
+  if(browser === "Safari" && parseInt(result.parsedResult.browser.version.slice(0,3)) <= 14.4){
+    alert("Version antigua de Safari en movil")
+  }
 }
 
+//chrome 83 abajo
+//edge 83 abajo
+//firefox 74
+//Safari 14 abajo
+//Safari mobile 14.4 abajo
+//Opera 72 abajo
 
-     alert("Name:"+browserName+" Version "+majorVersion)
 
-
-     //chrome 83 abajo
-     //edge 83 abajo
-     //firefox 74
+    
